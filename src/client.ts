a716cf261e4ad20d7ac0c4f0ad8505a499992e61
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-10-30 10:40:07 +0100

    Restructure and add ui to API interface

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,4 @@
+import { iZerionAPI, iZerionUI } from "./types/interface";
 import {
   ChainData,
   FungiblePositionsResponse,
@@ -5,94 +6,65 @@ import {
   GetPortfolioResponse,
   PortfolioData,
   PositionData,
+  UserDashboardResponse,
 } from "./types";
+import { transformPositionDataToUserDashboardResponse } from "./transform/ui";
+import { ZerionService } from "./services/zerion";
+import { ZERION_CONFIG } from "./config";
 
-const BASE_URL = "https://api.zerion.io/v1";
-
-// Utility function to make API requests with error handling
-async function fetchFromZerion<T>(
-  endpoint: string,
-  apiKey: string,
-  env?: string
-): Promise<T> {
-  const headers = {
-    accept: "application/json",
-    authorization: `Basic ${apiKey}`,
-    // Optionally add the "X-Env" header for testnet or other environments
-    ...(env ? { "X-Env": env } : {}),
-  };
-
-  const response = await fetch(`${BASE_URL}${endpoint}`, { headers });
-
-  if (!response.ok) {
-    throw new Error(`Failed to fetch ${endpoint}: ${response.statusText}`);
-  }
-  const data = await response.json();
-  return data as T;
-}
-
-export class ZerionAPI {
-  private apiKey: string;
-  env: string | undefined;
+export class ZerionAPI implements iZerionAPI {
+  service: ZerionService;
+  readonly ui: iZerionUI;
 
   constructor(apiKey: string, testnet: boolean) {
-    this.apiKey = apiKey;
-    this.env = testnet ? "testnet" : undefined;
+    this.service = new ZerionService(apiKey, testnet ? "testnet" : undefined);
+    this.ui = new ZerionUI(this);
   }
 
-  /**
-   * Fetches all supported blockchain chains from Zerion.
-   * @returns A promise resolving to an array of chains supported by Zerion.
-   */
   async getChains(): Promise<ChainData[]> {
-    const { data } = await fetchFromZerion<GetChainsResponse>(
-      "/chains/",
-      this.apiKey,
-      this.env
-    );
+    const { data } =
+      await this.service.fetchFromZerion<GetChainsResponse>("/chains/");
     return data;
   }
 
-  /**
-   * Fetches the portfolio of a specific wallet, partitioned by network.
-   * @param walletAddress The wallet address whose portfolio will be fetched.
-   * @param apiKey The Zerion API key for authorization.
-   * @param currency Optional currency code to get portfolio values (e.g., 'usd').
-   * @returns A promise resolving to the portfolio data for the wallet.
-   */
   async getPortfolio(
     walletAddress: string,
     currency: string = "usd"
   ): Promise<PortfolioData> {
-    const { data } = await fetchFromZerion<GetPortfolioResponse>(
-      `/wallets/${walletAddress}/portfolio?currency=${currency}`,
-      this.apiKey,
-      this.env
+    const { data } = await this.service.fetchFromZerion<GetPortfolioResponse>(
+      `/wallets/${walletAddress}/portfolio?currency=${currency}`
     );
     return data;
   }
 
-  /**
-   * Fetches the fungible token positions of a specific wallet.
-   * @param walletAddress The wallet address whose fungible positions will be fetched.
-   * @param currency Optional currency code to get token values (e.g., 'usd').
-   * @param filterPositions Optional filter to include only simple or specific positions.
-   * @param filterTrash Optional filter to exclude positions marked as trash.
-   * @param sort Optional sorting parameter (e.g., 'value').
-   * @returns A promise resolving to the wallet's fungible token positions.
-   */
   async getFungiblePositions(
     walletAddress: string,
-    currency: string = "usd",
-    filterPositions: string = "only_simple",
-    filterTrash: string = "only_non_trash",
-    sort: string = "value"
+    currency: string = ZERION_CONFIG.DEFAULT_CURRENCY,
+    filterPositions: string = ZERION_CONFIG.DEFAULT_FILTER,
+    filterTrash: string = ZERION_CONFIG.DEFAULT_TRASH_FILTER,
+    sort: string = ZERION_CONFIG.DEFAULT_SORT
   ): Promise<PositionData[]> {
-    const { data } = await fetchFromZerion<FungiblePositionsResponse>(
-      `/wallets/${walletAddress}/positions/?filter[positions]=${filterPositions}&currency=${currency}&filter[trash]=${filterTrash}&sort=${sort}`,
-      this.apiKey,
-      this.env
-    );
+    const { data } =
+      await this.service.fetchFromZerion<FungiblePositionsResponse>(
+        `/wallets/${walletAddress}/positions/?filter[positions]=${filterPositions}&currency=${currency}&filter[trash]=${filterTrash}&sort=${sort}`
+      );
     return data;
+  }
+}
+
+export class ZerionUI implements iZerionUI {
+  private client: ZerionAPI;
+
+  constructor(client: ZerionAPI) {
+    this.client = client;
+  }
+
+  async getUserBalances(walletAddress: string): Promise<UserDashboardResponse> {
+    const [chains, positions] = await Promise.all([
+      this.client.getChains(),
+      this.client.getFungiblePositions(walletAddress),
+    ]);
+
+    return transformPositionDataToUserDashboardResponse(positions, chains);
   }
 }
