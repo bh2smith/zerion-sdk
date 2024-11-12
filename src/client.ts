4638f428f2b0ccc40da587d44e060bc7db8ed959
Till <till.friesewinkel@googlemail.com>
2024-11-12 15:03:33 +0000

    Hardcode chain data

--- src/client.ts
+++ src/client.ts
@@ -4,7 +4,6 @@ import {
   FungiblePositionsResponse,
   FungibleResponse,
   FungibleTokenData,
-  GetChainsResponse,
   GetPortfolioResponse,
   PortfolioData,
   PositionData,
@@ -16,7 +15,7 @@ import {
   NFTPositionOptions,
 } from "./types";
 import { transformPositionDataToUserDashboardResponse } from "./transform/ui";
-import { ZerionService } from "./services/zerion";
+import { CHAINS, ZerionService } from "./services/zerion";
 import { DEFAULT_FUNGIBLE_OPTIONS } from "./config";
 import { buildQueryString } from "./util";
 
@@ -32,9 +31,7 @@ export class ZerionAPI implements iZerionAPI {
   }
 
   async getChains(): Promise<ChainData[]> {
-    const { data } =
-      await this.service.fetchFromZerion<GetChainsResponse>("/chains/");
-    return data;
+    return CHAINS;
   }
 
   async getPortfolio(
